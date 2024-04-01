ed9f540f29ad1a6bebc0604adedfc44e6cc85176
Harshit Tomar <harshitt345@gmail.com>
2024-04-01 15:15:16 +0530

    add diamond support

--- main.py
+++ main.py
@@ -35,3 +35,26 @@ class printShapes:
 			# 	def monotonic(h,b,value=0,spacing=2):
 			# 		for i in range(h,1,-1):
 			# 			print((str(value)+(" "*spacing))*(int(b*(i)/h)))
+
+	def diamond(d, value=0, spacing=2):
+		if(d%2):
+			for i in range(d):
+				if(i<=(d-1)/2):
+					print(" "*((1+spacing)*(int((d-1)/2)-i))+(str(value)+" "*spacing)*((2*i)+1))
+				else:
+					print(" "*((1+spacing)*(i-int((d-1)/2)))+(str(value)+" "*spacing)*(2*d-2*i-1))
+		else:
+			for i in range(d):
+				if(i<d/2):
+					print(" "*(1+spacing)*int(d/2-i-1)+(str(value)+" "*spacing)*2*(i+1)) 
+				else:
+					print(" "*(1+spacing)*(i-int(d/2))+(str(value)+" "*spacing)*2*(d-i)) 
+
+
+						
+		
+
+
+# printShapes.rectangle.monotonic()
+# printShapes.triangle.right.top.monotonic(5, 12,5)
+# printShapes.diamond(11)
